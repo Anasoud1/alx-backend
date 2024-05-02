eacca647ef129951c6ecfeb6aca9f1706956d4ca
Anasou1 <anasoudsassi@gmail.com>
2024-05-02 17:05:40 +0100

    adding 0-simple_helper_function.py file

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""index_range module"""
+
+
+def index_range(page, page_size):
+    """return a tuple of size two containing a start index and an end index"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
