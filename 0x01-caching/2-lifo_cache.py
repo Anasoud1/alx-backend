2741a0039ad8083f62b08dd2c69818a4277521e8
Anasou1 <anasoudsassi@gmail.com>
2024-05-08 13:09:42 +0100

    adding 2-lifo_cache.py file

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" lIFOCache module """
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Define LIFOCache"""
+
+    def __init__(self):
+        """constructor"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                last_item = list(self.cache_data)[-2]
+                del self.cache_data[last_item]
+                print(f"DISCARD: {last_item}")
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
