b6d3a4010a856311067e0b17c0c8c2b04e377e73
Anasou1 <anasoudsassi@gmail.com>
2024-05-08 16:28:49 +0100

    updating 4-mru_cache.py file

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -8,25 +8,27 @@ class MRUCache(BaseCaching):
 
     def __init__(self):
         """constructor"""
-        self.queue = []
+        self.stack = []
         super().__init__()
 
     def put(self, key, item):
         """Add an item in the cache"""
         if key and item:
             if self.cache_data.get(key):
-                self.queue.remove(key)
-            self.queue.append(key)
+                self.stack.remove(key)
+            while len(self.stack) >= self.MAX_ITEMS:
+                idx_del = self.stack.pop()
+                self.cache_data.pop(idx_del)
+                print(f'DISCARD: {idx_del}')
+
+            self.stack.append(key)
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                idx_del = self.queue.pop(-2)
-                del self.cache_data[idx_del]
 
     def get(self, key):
         """Get an item by key"""
         if key is None or key not in self.cache_data:
             return None
-        if key in self.queue:
-            self.queue.remove(key)
-            self.queue.append(key)
+        if key in self.stack:
+            self.stack.remove(key)
+            self.stack.append(key)
         return self.cache_data[key]
