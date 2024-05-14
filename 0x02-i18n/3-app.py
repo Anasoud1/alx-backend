b335e025a41927ffbe4a91dbe684d1e87076d694
Anasou1 <anasoudsassi@gmail.com>
2024-05-14 17:30:54 +0000

    adding 3-app.py file and tranlations folder

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""basic flask app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """Flask Babel configuration"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """function to determine the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def get_index3():
+    """return index page"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
